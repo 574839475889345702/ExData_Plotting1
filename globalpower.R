63c3c29f3a47a040f3b3a6a643218c5621de0871
mike <shortymike007@gmail.com>
2015-09-13 14:57:45 -0700

    change globalpower to generate all the plots.

--- globalpower.R
+++ globalpower.R
@@ -5,8 +5,13 @@ source("plot4.R")
 
 globalPowerGraphs<- function()
 {
-  power <- read.csv("household_power_consumption.txt",sep=";",stringsAsFactors=FALSE,na.strings = "?")
+  power <- read.csv("data/household_power_consumption.txt",sep=";",stringsAsFactors=FALSE,na.strings = "?")
   power$Date <- as.Date(power$Date, "%d/%m/%Y")
   subpower$Time <- strptime(paste(subpower$Date,subpower$Time), "%Y-%m-%d %H:%M:%S")
   subpower <- subset(power,Date <= as.Date("2007-02-02") & Date >= as.Date("2007-02-01")  )
+  
+  plot1()
+  plot2()
+  plot3()
+  plot4()
 }
