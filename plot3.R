bbb7859cd4fa8743ee82f71fe241c161712794dd
mike <shortymike007@gmail.com>
2015-09-13 14:28:02 -0700

    create xy plot of sub metered energy.

---
+++ plot3.R
@@ -0,0 +1,11 @@
+# Generate Plot of Energy Sub Metering
+
+png(filename = "plot3.png")
+
+with(subpower,plot(subpower$Time, subpower$Sub_metering_1, ylab = "Energy Sub Metering", xlab = "", type = "n"))
+with(subpower,lines(subpower$Time, subpower$Sub_metering_1, col = "black"))
+with(subpower,lines(subpower$Time, subpower$Sub_metering_2, col = "red"))
+with(subpower,lines(subpower$Time, subpower$Sub_metering_3, col = "blue"))
+legend("topright", lty = 1, col = c("black","red","blue"), legend = c("Sub Metering 1","Sub Metering 2","Sub Metering 3"))
+
+dev.off()
